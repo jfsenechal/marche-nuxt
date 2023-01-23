2aa03155ee9be8614de33b9c0134c21da067be39
jfsenechal <jfsenechal@gmail.com>
2023-01-23 15:14:25 +0100

    link tests

--- app/router.options.ts
+++ app/router.options.ts
@@ -4,13 +4,13 @@ export default <RouterConfig>{
     routes: (_routes) => [
         {
             name: 'article',
-            path: '/:categories([\/a-z]+)\/:articleSlug([a-z]+-):articleId(\\d+$)',
+            path: '/:categories([\/a-z]+)\/:articleSlug([a-z-]+-):articleId(\\d+$)',
             component: () => import('~/pages/article.vue')
         },
         {
             name: 'category',
-            path: '/:categories([\/a-z]+)\/:categorySlug(\\w+)',
-            component: () => import('~/pages/category.vue')//([\a-z]+)\/(?:[a-z]+-)(\d+)$
+            path: '/:categories([\/a-z-]+)\/:categorySlug(\\w+)',
+            component: () => import('~/pages/category.vue')
         },
         {
             name: 'culture',
@@ -34,3 +34,12 @@ export default <RouterConfig>{
 //([a-zA-Z0-9-]+)(?:[\/]?)$ capture le dernier element de l'url
 
 // la fin d'url avec un tiret et un ou plusiers chiffres (?:-)(\d+)(?:\/?)$ capture 23
+
+// http://localhost:3000/ze-ze/zozo/mu
+// http://localhost:3000/zeze/zozo/zizi/mu-23
+// http://localhost:3000/memo/lolo/bculture/culture/bibliotheque-et-ludotheque-provinciales-du-luxembourg-23
+// http://localhost:3000/ze-ze/zozo/mu
+// http://localhost:3000/sante/centres-medicaux-services/
+// http://localhost:3000/famille/naissance/consultations-one-205
+// http://localhost:3000/famille/naissance/consultations-one-205/
+// http://localhost:3000/sante/centres-medicaux-services/hopital-princesse-paola-ifac-vivalia-8

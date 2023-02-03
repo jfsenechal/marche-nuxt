a04a8c30b85ab76f446ded9e31fd2a940ce9ed4a
jfsenechal <jfsenechal@gmail.com>
2023-02-03 22:39:56 +0100

    details

--- pages/article.vue
+++ pages/article.vue
@@ -1,15 +1,15 @@
 <script setup>
 const {path, params} = useRoute()
-const siteSlug = computed(() => String(params.siteSlug || 'pas slug'))
+const siteSlug = computed(() => String(params.siteSlug || 'citoyen'))
 const articleId = computed(() => Number(params.articleId || 0))
-const articleSlug = computed(() => String(params.articleSlug || 'no slug'))
+const articleSlug = computed(() => String(params.articleSlug || 'no article slug'))
 const categories = computed(() => String(params.categories || 'no cats'))
+
 const {
   pending2,
   data: article,
   error
 } = useLazyFetch(`https://www.marche.be/api/post.php?site=${siteSlug.value}&id=${articleId.value}`)
-console.log(`https://www.marche.be/api/post.php?id=${articleId.value}&site=${siteSlug.value}`)
 watch(article, (newPost) => {
   console.log(article.value)
 })
@@ -29,9 +29,22 @@ useHead({
     <div v-if="error">
       Error {{ error }}
     </div>
-    <div v-else>
-      <h3 class="text-2xl font-bold">{{ article.post_title ?? 'Article has no title' }}</h3>
+    <div v-if="article">
+      <h3 class="text-2xl font-bold">{{ article.post_title ?? 'bug post title' }}</h3>
       <div v-html="article.post_content ?? 'no content'"></div>
     </div>
+    <div v-else>
+      <h3 class="text-2xl font-bold">Article not found</h3>
+    </div>
+    <a class="mt-3 block" href="https://www.marche.be/api/post.php?id={{articleId}}&site={{siteSlug}}">
+      https://www.marche.be/api/post.php?id={{ articleId }}&site={{ siteSlug }}
+    </a>
+
+    <ul class="mt-3 border border-administration">
+      <li class="p-3">site slug: {{ siteSlug }}</li>
+      <li class="p-3">article slug: {{ articleSlug }}</li>
+      <li class="p-3">article id: {{ articleId }}</li>
+    </ul>
+
   </section>
 </template>

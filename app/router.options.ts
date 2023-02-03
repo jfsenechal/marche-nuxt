168ced74bd7b304a879f25d90a719d9b5beb2fc0
jfsenechal <jfsenechal@gmail.com>
2023-02-03 22:39:44 +0100

    real urls sample

--- app/router.options.ts
+++ app/router.options.ts
@@ -18,6 +18,16 @@ export default <RouterConfig>{
             component: () => import('~/pages/site.vue')
         },
         {
+            name: 'citoyen-article',
+            path: '/:categories([a-z-_]+)+\/:articleSlug([a-z-]+-):articleId(\\d+$)',
+            component: () => import('~/pages/article.vue')
+        },
+        {
+            name: 'citoyen-category',
+            path: '/:categories([a-z-_]+)+\/:categorySlug(\\w+)',
+            component: () => import('~/pages/category.vue')
+        },
+        {
             name: 'homepage',
             path: '/',
             component: () => import('~/pages/homepage.vue')
@@ -32,18 +42,18 @@ export default <RouterConfig>{
 ///culture/culture/bibliotheque-et-ludotheque-provinciales-du-luxembourg-23/
 //^(?:\/)([a-zA-Z0-9-]+) capture site
 //([a-zA-Z0-9-]+)(?:[\/]?)$ capture le dernier element de l'url
-
 // la fin d'url avec un tiret et un ou plusiers chiffres (?:-)(\d+)(?:\/?)$ capture 23
 
-// http://localhost:3000/sport/zozo/mu
-// http://localhost:3000/social/zozo/zizi/mu-23
-// http://localhost:3000/culture/lolo/li-li/bculture/culture/bibliotheque-et-ludotheque-provinciales-du-luxembourg-23
-// http://localhost:3000/culture/ze-ze/zo_zo/mu
-// http://localhost:3000/sante/centres-medicaux-services/
-// http://localhost:3000/famille/naissance/consultations-one-205
+// http://localhost:3000/commissions-consultatives/
+// http://localhost:3000/cultes-laicite-cimetieres/
 // http://localhost:3000/famille/naissance/consultations-one-205/
+// http://localhost:3000/emploi-formation/enseignement-formation-pour-adultes/centre-de-formation-informatique-244/
+// http://localhost:3000/emploi-formation/enseignement-formation-pour-adultes/formation-daccueillante-extra-scolaire-690/
+
+// http://localhost:3000/social/centre-public-daction-sociale-cpas/le-service-social-3901/
+// http://localhost:3000/culture/lolo/li-li/bculture/culture/bibliotheque-et-ludotheque-provinciales-du-luxembourg-23
+// http://localhost:3000/sante/centres-medicaux-services/
 // http://localhost:3000/sante/centres-medicaux-services/hopital-princesse-paola-ifac-vivalia-8
-// http://localhost:3000/ze-ze/zozo/sport
-// http://localhost:3000/ze-ze/zozo/sport/
-// http://localhost:3000/ze-ze/zozo/enfance-jeunesse
-// http://localhost:3000/famille/naissance/consultations-one-205
+// http://localhost:3000/administration/subventions-et-primes-communales/prime-recyparc-tric-et-vrac-2022-1806/
+// http://localhost:3000/administration/les-services-communaux/adl-agence-de-developpement-local-72/
+// http://localhost:3000/sante/cellule-communale-sante-viasano/semaine-du-commerce-equitable-11427/

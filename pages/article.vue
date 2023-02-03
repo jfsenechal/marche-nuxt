cb877a4369cbb13b2622525b7ec36b2dfd481fc7
jfsenechal <jfsenechal@gmail.com>
2023-02-03 22:03:09 +0100

    new params

--- pages/article.vue
+++ pages/article.vue
@@ -1,9 +1,9 @@
 <script setup>
-const route = useRoute()
-const siteSlug = computed(() => String(route.params.siteSlug || 'pas slug'))
-const articleId = computed(() => Number(route.params.articleId || 0))
-const articleSlug = computed(() => String(route.params.articleSlug || 'no slug'))
-const categories = computed(() => String(route.params.categories || 'no cats'))
+const {path, params} = useRoute()
+const siteSlug = computed(() => String(params.siteSlug || 'pas slug'))
+const articleId = computed(() => Number(params.articleId || 0))
+const articleSlug = computed(() => String(params.articleSlug || 'no slug'))
+const categories = computed(() => String(params.categories || 'no cats'))
 const {
   pending2,
   data: article,
@@ -11,9 +11,14 @@ const {
 } = useLazyFetch(`https://www.marche.be/api/post.php?site=${siteSlug.value}&id=${articleId.value}`)
 console.log(`https://www.marche.be/api/post.php?id=${articleId.value}&site=${siteSlug.value}`)
 watch(article, (newPost) => {
-  console.log(article, newPost)
-  console.log(pending2)
-  console.log(newPost)
+  console.log(article.value)
+})
+useHead({
+  titleTemplate: (title) => `${title} | Marche.be`,
+  script: [{
+    //  src: '',
+    //  tagPosition: "bodyClose"
+  }]
 })
 </script>
 <template>
@@ -25,7 +30,7 @@ watch(article, (newPost) => {
       Error {{ error }}
     </div>
     <div v-else>
-      <h3 class="text-2xl font-bold">{{ article.post_title ?? 'no title' }}</h3>
+      <h3 class="text-2xl font-bold">{{ article.post_title ?? 'Article has no title' }}</h3>
       <div v-html="article.post_content ?? 'no content'"></div>
     </div>
   </section>

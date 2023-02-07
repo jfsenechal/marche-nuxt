0b870b5db2d5fceabc228eb8be7a64648485cc74
jfsenechal <jfsenechal@gmail.com>
2023-02-07 22:16:58 +0100

    citoyen cat alone ok

--- app/router.options.ts
+++ app/router.options.ts
@@ -24,12 +24,7 @@ export default <RouterConfig>{
         },
         {
             name: 'citoyen-category',
-            path: '/:categories([a-z-_]+)+\/:categorySlug(\\w+)',
-            component: () => import('~/pages/category.vue')
-        },
-        {
-            name: 'citoyen-category-alone',
-            path: '/:categorySlug(\\w+)',
+            path: '/:categories([a-z-_]+)*\/:categorySlug([a-zA-Z0-9-_]+)',
             component: () => import('~/pages/category.vue')
         },
         {
@@ -49,19 +44,19 @@ export default <RouterConfig>{
 //([a-zA-Z0-9-]+)(?:[\/]?)$ capture le dernier element de l'url
 // la fin d'url avec un tiret et un ou plusiers chiffres (?:-)(\d+)(?:\/?)$ capture 23
 
-// http://localhost:3000/commissions-consultatives/
-// http://localhost:3000/cultes-laicite-cimetieres/
-// http://localhost:3000/famille/naissance/consultations-one-205/
-// http://localhost:3000/securite-secours/police-locale-303/
-// http://localhost:3000/emploi-formation/enseignement-formation-pour-adultes/formation-daccueillante-extra-scolaire-690/
-// http://localhost:3000/environnement/dechets/cadavres-de-petits-animaux-147/
-// http://localhost:3000/social/centre-public-daction-sociale-cpas/le-service-social-3901/
-// http://localhost:3000/social/centre-public-daction-sociale-cpas/le-service-social-3901
-// http://localhost:3000/administration/le-college-communal/
-// http://localhost:3000/culture/lolo/li-li/bculture/culture/bibliotheque-et-ludotheque-provinciales-du-luxembourg-23
-// http://localhost:3000/sante/centres-medicaux-services/
-// http://localhost:3000/sante/centres-medicaux-services/hopital-princesse-paola-ifac-vivalia-8
-// http://localhost:3000/administration/subventions-et-primes-communales/prime-recyparc-tric-et-vrac-2022-1806/
-// http://localhost:3000/administration/les-services-communaux/adl-agence-de-developpement-local-72/
-// http://localhost:3000/sante/cellule-communale-sante-viasano/semaine-du-commerce-equitable-11427/
-// http://localhost:3000/sport/actualites/acces-piscine-centre-sportif-pour-le-public-5153/
+// http://nuxt.local/commissions-consultatives/
+// http://nuxt.local/cultes-laicite-cimetieres/
+// http://nuxt.local/famille/naissance/consultations-one-205/
+// http://nuxt.local/securite-secours/police-locale-303/
+// http://nuxt.local/emploi-formation/enseignement-formation-pour-adultes/formation-daccueillante-extra-scolaire-690/
+// http://nuxt.local/environnement/dechets/cadavres-de-petits-animaux-147/
+// http://nuxt.local/social/centre-public-daction-sociale-cpas/le-service-social-3901/
+// http://nuxt.local/social/centre-public-daction-sociale-cpas/le-service-social-3901
+// http://nuxt.local/administration/le-college-communal/
+// http://nuxt.local/culture/lolo/li-li/bculture/culture/bibliotheque-et-ludotheque-provinciales-du-luxembourg-23
+// http://nuxt.local/sante/centres-medicaux-services/
+// http://nuxt.local/sante/centres-medicaux-services/hopital-princesse-paola-ifac-vivalia-8
+// http://nuxt.local/administration/subventions-et-primes-communales/prime-recyparc-tric-et-vrac-2022-1806/
+// http://nuxt.local/administration/les-services-communaux/adl-agence-de-developpement-local-72/
+// http://nuxt.local/sante/cellule-communale-sante-viasano/semaine-du-commerce-equitable-11427/
+// http://nuxt.local/sport/actualites/acces-piscine-centre-sportif-pour-le-public-5153/

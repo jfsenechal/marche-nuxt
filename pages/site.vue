cfe164261954878176ce36a54dd081479f772617
jfsenechal <jfsenechal@gmail.com>
2023-02-12 15:45:03 +0100

    working parents

--- pages/site.vue
+++ pages/site.vue
@@ -1,23 +1,29 @@
 <script setup>
 const route = useRoute()
-const siteName = route.name
-const siteSlug = computed(() => String(route.params.siteSlug || 'pas slug'))
+const siteSlug = computed(() => String(route.params.siteSlug || 'citoyen'))
 const {pending, data: item, error} = useLazyFetch(`https://www.marche.be/api/menu.php?site=${siteSlug.value}`)
+const parents = computed((item) => [])
+
 </script>
 <template>
   <section>
     <h3 class="text-2xl font-bold">Index du site {{ siteSlug }}</h3>
+    <p>
+      <a :href='`https://www.marche.be/api/menu.php?site=${siteSlug}`'>
+        https://www.marche.be/api/menu.php?site={{ siteSlug }}
+      </a>
+    </p>
     <div v-if="pending">
       Loading Menu...
     </div>
     <div v-else>
       <ul class="flex flex-col gap-2">
-        <li v-for="child in item.items" :key="child.ID">
+        <li v-for="child in item.items" :key="child.ID" class="p-1">
           <NuxtLink :to="{
                   name: 'category',
                   params:{
                       siteSlug:siteSlug,
-                      categories:'environnement',
+                      categories:child.parents,
                       categorySlug: child.slug ?? 'slugfound' }
           }" v-if="child.object === 'category' ">
             {{ child.object }}: {{ child.title }}
@@ -26,8 +32,8 @@ const {pending, data: item, error} = useLazyFetch(`https://www.marche.be/api/men
                    name: 'article',
                    params:{
                       siteSlug:siteSlug,
-                      categories:'environnement',
-                      articleSlug: child.post_name + '-' ?? 'SlugNotFound',
+                      categories:child.parents,
+                      articleSlug: child.slug + '-' ?? 'slugNotFound',
                       articleId: child.ID ?? 'IdNotFound' }
           }" v-if="child.object === 'post' || child.object === 'page'">
             {{ child.object }}: {{ child.title }}

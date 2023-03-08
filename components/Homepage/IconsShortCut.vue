b20eafcc86b0cfc36f0bc276b2d7a40a9298688c
jfsenechal <jfsenechal@gmail.com>
2023-03-08 21:26:23 +0100

    set composables

--- components/Homepage/IconsShortCut.vue
+++ components/Homepage/IconsShortCut.vue
@@ -1,73 +1,23 @@
 <script setup>
-const icones = [
-  {
-    title: "Piscine",
-    icon: "i-swimmer",
-    iconHover: "group-hover:i-swimmer-white",
-    id: 1,
-    url: "/",
-  },
-  {
-    title: "Environnement <br/>Déchet",
-    icon: "i-leaf",
-    iconHover: "group-hover:i-leaf-white",
-    id: 2,
-    url: "/",
-  },
-  {
-    title: "Travaux <br/>Arrêtés de Police",
-    icon: "i-traffic-cone",
-    iconHover: "group-hover:i-traffic-cone-white",
-    id: 3,
-    url: "/",
-  },
-  {
-    title: "Enfance<br/>Jeunesse",
-    icon: "i-beach-ball",
-    iconHover: "group-hover:i-beach-ball-white",
-    id: 4,
-    url: "/",
-  },
-  {
-    title: "Carte dynamique",
-    icon: "i-map",
-    iconHover: "group-hover:i-map-white",
-    id: 5,
-    url: "/",
-  },
-  {
-    title: "CPAS",
-    icon: "i-handshake",
-    iconHover: "group-hover:i-handshake-white",
-    id: 6,
-    url: "/",
-  },
-  {
-    title: "@Marche.be",
-    icon: "i-envelope",
-    iconHover: "group-hover:i-envelope-white",
-    id: 7,
-    url: "/",
-  },
-];
+import {icons} from "~/composables/iconsHomeGet";
 </script>
 <template>
   <ul class="my-8 flex flex-wrap justify-center gap-2">
     <li
-      v-for="item in icones"
-      :id="item.id"
-      :key="item.id"
-      class="group flex flex-shrink-0 flex-grow-0 basis-1/5 justify-center text-center"
+        v-for="item in icons"
+        :id="item.id"
+        :key="item.id"
+        class="group flex flex-shrink-0 flex-grow-0 basis-1/5 justify-center text-center"
     >
       <a :href="item.url" class="icon_custom">
         <i
-          class="mesicones mx-auto block h-12 w-12 rounded-full bg-white bg-[length:1.25rem_1.25rem] bg-center bg-no-repeat shadow-xl hover:text-white group-hover:bg-cta-light xl:h-14 xl:w-14 xl:bg-[length:2.25rem_2.25rem]"
-          :class="item.icon + ' ' + item.iconHover"
+            class="mesicones mx-auto block h-12 w-12 rounded-full bg-white bg-[length:1.25rem_1.25rem] bg-center bg-no-repeat shadow-xl hover:text-white group-hover:bg-cta-light xl:h-14 xl:w-14 xl:bg-[length:2.25rem_2.25rem]"
+            :class="item.icon + ' ' + item.iconHover"
         >
         </i>
         <span
-          class="mx-auto inline-block pt-3 font-montserrat-semi-bold text-sm text-white xl:text-base xl:leading-4"
-          v-html="item.title"
+            class="mx-auto inline-block pt-3 font-montserrat-semi-bold text-sm text-white xl:text-base xl:leading-4"
+            v-html="item.title"
         ></span>
       </a>
     </li>
---
+++ composables/widgetsHomeGet.ts
@@ -0,0 +1,38 @@
+export const widgets = [
+    {
+        title: "Avis de publication",
+        intro: "Enquêtes publiques, <br>assemblées institutions...",
+        class: "bg-img-widgets-1",
+        url: "/",
+        id: 1,
+    },
+    {
+        title: "Economie",
+        intro: "Infos pratiques, <br>commerces locaux...",
+        class: "bg-img-widgets-2",
+        url: "/",
+        id: 2,
+    },
+    {
+        title: "Participation citoyenne",
+        intro:
+            "Plateforme de lenveloppe participative,<br> consultations publiques...",
+        class: "bg-img-widgets-3",
+        url: "/",
+        id: 3,
+    },
+    {
+        title: "Plateforme de volontariat",
+        intro: "Offres et demandes du tissu associatif",
+        class: "bg-img-widgets-4",
+        url: "/",
+        id: 4,
+    },
+    {
+        title: "Avis de décès",
+        intro: "Annonces nécrologiques <br> de notre commune",
+        class: "bg-img-widgets-5",
+        url: "/",
+        id: 5,
+    }
+];

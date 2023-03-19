4d080dc74ae01808a28b2ed2c23f6ff37ae44ed5
jfsenechal <jfsenechal@gmail.com>
2023-03-19 18:58:46 +0100

    add

---
+++ components/Widget/Error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="prose p-12 bg-white rounded-md w-[65ch]">
+    <NuxtErrorBoundary>
+      <NuxtPage/>
+      <template #error="{ error }">
+        <p>
+          Oh no, something went wrong with the lesson!
+          <code>{{ error }}</code>
+        </p>
+        <p>
+          <button
+              class="hover:cursor-pointer bg-gray-500 text-white font-bold py-1 px-3 rounded"
+              @click="resetError(error)"
+          >
+            Reset
+          </button>
+        </p>
+      </template>
+    </NuxtErrorBoundary>
+  </div>
+</template>
+<script setup>
+const resetError = async (error) => {
+  await navigateTo(
+      'home'
+  );
+  error.value = null;
+};
+</script>
---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <div class="">
+        error {{ error.message }}
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
+<script setup>
+const error = useError
+</script>

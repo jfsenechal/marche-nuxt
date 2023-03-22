8218aed2b454a785bd2696cc15ece0eec791272d
jfsenechal <jfsenechal@gmail.com>
2023-03-22 19:48:35 +0100

    extract items

--- components/Header/NavItemsXl.vue
+++ components/Header/NavItemsXl.vue
@@ -1,28 +1,17 @@
 <script setup>
+import {items} from "~/composables/menuItemsGet";
 defineEmits(["open-search"]);
 </script>
 <template>
-  <li class="hidden xl:list-item nav-top font-montserrat-semi-bold text-cta-dark">
-    <NuxtLink to="https://eguichet.marche.be">E-guichet</NuxtLink>
-  </li>
-  <li class="hidden xl:list-item nav-top font-montserrat-semi-bold">
-    <NuxtLink to="news">Actualités</NuxtLink>
-  </li>
-  <li class="hidden xl:list-item nav-top font-montserrat-semi-bold">
-    <NuxtLink to="agenda">Horaires-Contact</NuxtLink>
-  </li>
-  <li class="hidden xl:list-item nav-top font-montserrat-semi-bold">
-  <NuxtLink to="agenda">Agenda</NuxtLink>
-  </li>
-  <li class="hidden xl:list-item nav-top font-montserrat-semi-bold">
-    <NuxtLink to="agenda">Inondation</NuxtLink>
+  <li v-for="item in items" :key="item.id" class="hidden xl:list-item nav-top font-montserrat-semi-bold">
+    <NuxtLink :to="item.link">{{item.name}}</NuxtLink>
   </li>
   <li class="hidden xl:list-item nav-top font-montserrat-semi-bold" title="Rechercher">
     <a href="/" @click.prevent="$emit('open-search')">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
            stroke="currentColor" class="w-6 h-6 text-sante">
         <path stroke-linecap="round" stroke-linejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
       </svg>
     </a>
   </li>
---
+++ composables/menuItemsGet.ts
@@ -0,0 +1,7 @@
+export const items = [
+    {id: 1, name: 'E-guichet', link: 'https://eguichet.marche.be'},
+    {id: 2, name: 'Actualités', link: 'news'},
+    {id: 3, name: 'Horaires-Contact', link: 'agenda'},
+    {id: 4, name: 'Agenda', link: 'agenda'},
+    {id: 5, name: 'Inondation', link: 'agenda'}
+];

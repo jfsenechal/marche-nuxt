e74f8ed4124a37b2a968a151c83d8c1706d860b2
jfsenechal <jfsenechal@gmail.com>
2023-03-26 21:44:22 +0200

    new header

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -12,11 +12,12 @@ function closeSearch() {
 }
 </script>
 <template>
-  <header
-    class="fixed flex h-20 w-full items-center justify-end p-6 bg-white shadow-md z-20"
+  <header class="fixed px-3 bg-white shadow-md flex items-center justify-end w-full">
+<!--  <header
+    class="fixed flex h-20 w-full items-stretch justify-end p-6 bg-white shadow-md z-20"
     id="headerTop"
-  >
-    <a href="/" class="mr-auto">
+  >-->
+    <a href="/" class="mr-auto self-center">
       <img src="/images/img_logo.png" alt="logo" class="h-10 w-16 xl:h-14 xl:w-24" />
     </a>
     <Navigation @close-search=closeSearch @open-search="updateOpenSearch" />
--- components/Header/NavItemsXl.vue
+++ components/Header/NavItemsXl.vue
@@ -1,13 +1,19 @@
 <script setup>
 import {items} from "~/composables/menuItemsGet";
+
 defineEmits(["open-search"]);
 </script>
 <template>
-  <li v-for="item in items" :key="item.id" class="hidden xl:list-item nav-top font-montserrat-semi-bold">
-    <NuxtLink :to="item.link">{{item.name}}</NuxtLink>
+  <li v-for="item in items" :key="item.id" class="hidden px-3 xl:flex xl:items-stretch font-montserrat-semi-bold">
+    <NuxtLink
+        :to="item.link"
+        class="inline-flex items-center border-b-2 border-transparent hover:border-b-2 hover:border-cta-light  leading-6 text-cta-dark">
+      {{ item.name }}
+    </NuxtLink>
   </li>
-  <li class="hidden xl:list-item nav-top font-montserrat-semi-bold" title="Rechercher">
-    <a href="/" @click.prevent="$emit('open-search')">
+  <li class="hidden xl:flex xl:items-stretch font-montserrat-semi-bold" title="Rechercher">
+    <a href="/" @click.prevent="$emit('open-search')"
+       class="inline-flex items-center border-b-2 border-transparent hover:border-b-2 hover:border-cta-light  leading-6 text-cta-dark">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
            stroke="currentColor" class="w-6 h-6 text-sante">
         <path stroke-linecap="round" stroke-linejoin="round"

3901e925016ec78b78031cb8e824c7446406c20a
jfsenechal <jfsenechal@gmail.com>
2023-03-26 19:00:23 +0200

    add style

--- pages/draft.vue
+++ pages/draft.vue
@@ -13,23 +13,15 @@
                         enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150"
                         leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
               <PopoverPanel
-                  class="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                <div class="p-4">
+                  class="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-cta-dark shadow-lg ring-1 ring-gray-900/5">
+                <div class="p-2">
                   <div v-for="item in menu" :key="item.blogid"
-                       class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                    <div
-                        class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                      <component :is="Bars3Icon" class="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
-                                 aria-hidden="true"/>
-                    </div>
+                       class="group relative flex items-center gap-x-6 rounded-lg text-sm leading-6 hover:bg-gray-50">
                     <div class="flex-auto">
-                      <a href="/" class="block font-semibold text-gray-900">
+                      <a href="/" class="block font-semibold text-white p-3"
+                      :class="item.colorhover">
                         {{ item.name }}
-                        <span class="absolute inset-0"/>
                       </a>
-                      <p class="mt-1 text-gray-600">
-                        Build strategic funnels that will convert
-                      </p>
                     </div>
                   </div>
                 </div>

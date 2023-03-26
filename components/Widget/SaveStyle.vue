4e0ccdaf5ff2b9b2dd37a30317209afba0fb82ba
jfsenechal <jfsenechal@gmail.com>
2023-03-26 18:58:29 +0200

    save hover

---
+++ components/Widget/SaveStyle.vue
@@ -0,0 +1,11 @@
+<template>
+  <a href="#" class="hover:text-sport">coucou</a>
+  <a href="#" class="hover:text-sante">coucou</a>
+  <a href="#" class="hover:text-culture">coucou</a>
+  <a href="#" class="hover:text-enfance">coucou</a>
+  <a href="#" class="hover:text-tourisme">coucou</a>
+  <a href="#" class="hover:text-citoyen">coucou</a>
+  <a href="#" class="hover:text-administration">coucou</a>
+  <a href="#" class="hover:text-social">coucou</a>
+  <a href="#" class="hover:text-economie">coucou</a>
+</template>

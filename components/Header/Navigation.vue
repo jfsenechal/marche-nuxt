49f798b7c50c147f0cc2c42b91d9e53a9df85ae3
jfsenechal <jfsenechal@gmail.com>
2023-03-30 20:50:36 +0200

    fix bottom mobile

--- components/Header/Navigation.vue
+++ components/Header/Navigation.vue
@@ -25,26 +25,27 @@ function openMenuVivre() {
 }
 </script>
 <template>
-  <nav class="relative h-16 flex px-2 lg:px-8" aria-label="Global">
-    <ul class="flex items-stretch bg-cta-dark text-white xl:flex xl:bg-white xl:text-cta-dark"
+  <nav class="musshadow xl:shadow-none fixed bottom-0 top-auto right-0 left-0 xl:relative h-16 flex lg:px-8" aria-label="Global">
+    <!-- class="h-20 grid grid-cols-5 items-center justify-evenly bg-cta-dark text-white xl:flex xl:bg-white xl:text-cta-dark" -->
+    <ul class="grid grid-cols-5 w-full items-center justify-evenly bg-cta-dark text-white lg:justify-start lg:items-stretch xl:flex xl:bg-white xl:text-cta-dark"
         id="navigationTop">
       <NavIconHomeMobile/>
       <NavIconSearchMobile/>
       <li
-          class="hidden px-3 xl:flex xl:items-stretch xl:font-montserrat-semi-bold xl:transition-all xl:duration-500 btn-vivre">
+          class="px-3 xl:flex xl:items-stretch xl:font-montserrat-semi-bold xl:transition-all xl:duration-500 btn-vivre">
         <a href="#"
            class="hidden xl:inline-flex items-center border-b-2 border-transparent hover:border-b-2 hover:border-cta-light  leading-6 text-cta-dark">
           <IconMenuVivreSvg/>
           Vivre à Marche
         </a>
-        <label for="checkboxMenu" class="flex xl:hidden icon-menu-mobile cursor-pointer">
+        <label for="checkboxMenu" class="icon-menu-mobile cursor-pointer">
           <button name="menu" @click="openMenuVivre">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                  stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </button>
-          <span class="text-menu-mobile">Menu2</span>
+          <span class="text-menu-mobile">Menu</span>
         </label>
         <NavigationVivre/>
       </li>
@@ -60,7 +61,7 @@ function openMenuVivre() {
 }
 
 .icon-menu-mobile {
-  @apply my-4 flex-col flex-wrap items-center justify-center font-montserrat-semi-bold;
+  @apply my-4 flex flex-col flex-wrap items-center justify-center font-montserrat-semi-bold xl:hidden;
 }
 
 .text-menu-mobile {
@@ -68,8 +69,8 @@ function openMenuVivre() {
 }
 
 .musshadow {
-  /* box-shadow: 0 -0.125rem 0.45rem rgba(0, 0, 0, 0.75);
-   @apply lg:shadow-none*/
+   box-shadow: 0 -0.125rem 0.45rem rgba(0, 0, 0, 0.75);
+   @apply lg:shadow-none
 }
 
 </style>
